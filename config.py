269f38457c18812d9a8cf401e0afd707b9199d64
Lin En Tan <linentan04@gmail.com>
2026-02-05 11:40:00 -0600

    Set numbers to OS environment

--- config.py
+++ config.py
@@ -5,5 +5,8 @@ account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 client = Client(account_sid, auth_token)
 
+TO_NUMBER = os.environ["TO_NUMBER"]
+FROM_NUMBER = os.environ["FROM_NUMBER"]
+
 api_key = os.environ["OWM_API_KEY"]
 
