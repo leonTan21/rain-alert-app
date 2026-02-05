fe38560034ee4161a1eca33668ee7559ff4ec015
Lin En Tan <linentan04@gmail.com>
2026-02-05 11:18:47 -0600

    Implemented rain logic and message

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
-from api import api_key
+from config import api_key, client
 
 OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 
 weather_params = {
     "lat": 39.799358,
-    "long": -89.643623,
+    "lon": -89.643623,
     "appid": api_key,
     "cnt": 4,
 }
@@ -14,4 +14,16 @@ response = requests.get(OWM_endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
 
-print(weather_data)
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if condition_code < 700:
+        will_rain = True
+        break
+
+if will_rain:
+    message = client.messages.create(
+        to="+12172802180",
+        from_="+18445042644",
+        body="It will rain ☔"
+    )
