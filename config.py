0829145fc96d904fc4794e61029688fc334e6a13
Lin En Tan <linentan04@gmail.com>
2026-02-05 11:16:48 -0600

    Config updated

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from twilio.rest import Client
+
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+client = Client(account_sid, auth_token)
+
+api_key = os.environ["OWM_API_KEY"]
+
