3d2ea5af0f269f0800a39bfb1a4fd7c4534204a4
Lin En Tan <linentan04@gmail.com>
2026-02-09 11:02:11 -0600

    Updated config to match proxy client for twillio free users

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
 from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+proxy_client = TwilioHttpClient()
+proxy_client.session.proxies = {'https': os.environ['https_proxy']}
 
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-client = Client(account_sid, auth_token)
+client = Client(account_sid, auth_token, http_client=proxy_client)
 
 TO_NUMBER = os.environ["TO_NUMBER"]
 FROM_NUMBER = os.environ["FROM_NUMBER"]
